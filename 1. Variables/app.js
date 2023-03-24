cef2f09e0615458f5232ad1f4444fea18d9ffb48
Adriana Lubniewski <alubni@hotmail.com>
2023-03-24 13:57:28 -0300

    CSS

--- 1. Variables/app.js
+++ 1. Variables/app.js
@@ -108,35 +108,3 @@
 
 // // adivinaElNumero()
 
-/* saludo
-
-Hacer un programa que al iniciarse pida mediante un prompt ingresar un nombre, y una vez ingresado muestre un saludo con el nombre dentro de un h1. El h1 debe estar centrado y tener un tamaño de letra y una tipografía distinta de la que viene por defecto. */
-
-// const nombre = prompt("Ingresa tu nombre")
-
-
-// const h1 = document.querySelector("h1")
-
-// h1.innerText += nombre
-// h1.style.textAlign = "center"
-// h1.style.color = "red"
-// h1.style.fontFamily = "Arial"
-
-// console.log("hola")
-
-/* color
-Hacer un programa que al iniciarse pida mediante un prompt ingresar un color en sistema hexadecimal, y una vez ingresado ponga el color de fondo del body con dicho color. */
-/*  const color= prompt("Ingresa un color en sistema hexadecimal,  #0000")
- const body= document.querySelector("body")
- body.style.backgroundColor = color */
- /* rgb
-Hacer un programa que al iniciarse pida mediante tres prompts ingresar los valores correspondientes a un color en sistema rgb, y una vez ingresado ponga el color de fondo del body con dicho color. Los valores van del 0 al 255 y corresponden a rojo, verde y azul. */
-
-const red = parseInt(prompt("Ingrese un numero entre 0 y 255"))
-const green = parseInt(prompt("Ingrese un numero entre 0 y 255"))
-const blue = parseInt(prompt("Ingrese un numero entre 0 y 255"))
-const color= `rgb(${red} ${green} ${blue})`
-
-
-const body = document.querySelector("body")
-body.style.background = color
