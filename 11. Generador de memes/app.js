127835f24240dfb37b30e6c2963c1ca17cf73848
Adriana Lubniewski <alubni@hotmail.com>
2023-03-26 00:51:59 -0300

    filters, reset button

--- 11. Generador de memes/app.js
+++ 11. Generador de memes/app.js
@@ -1,7 +1,7 @@
 
 const $ = (selector) => document.querySelector(selector);
 
-///boton imagen
+ ///boton imagen
 const imageBtn= $(".imageBtn")
 const textBtn= $(".textBtn")
 
@@ -40,3 +40,55 @@ darkButton.addEventListener("click", ()=>{
     darkButton.classList.add("display-none")
     document.body.classList.add("dark")
 })
+
+// filtros
+
+const brightness=$("#brightness")
+const opacity=$("#opacity" )
+const contrast=$("#contrast" )
+const blur=$("#blur")
+const grayscale=$("#grayscale")
+const sepia=$("#sepia")
+const hue=$("#hue")
+const saturate=$("#saturate")
+const invert=$("#invert")
+const applyFilters= () =>{
+    const filters= `brightness(${brightness.value}) opacity(${opacity.value}) contrast(${contrast.value}%) blur(${blur.value}px) grayscale(${grayscale.value}%) sepia(${sepia.value}%) hue-rotate(${hue.value}deg) saturate(${saturate.value}%) invert(${invert.value})`;
+
+
+    $(".meme-image").style.filter= filters
+}
+
+brightness.addEventListener("input", applyFilters)
+opacity.addEventListener("input", applyFilters)
+contrast.addEventListener("input", applyFilters)
+blur.addEventListener("input", applyFilters)
+grayscale.addEventListener("input", applyFilters)
+sepia.addEventListener("input", applyFilters)
+hue.addEventListener("input", applyFilters)
+saturate.addEventListener("input", applyFilters)
+invert.addEventListener("input", applyFilters)
+
+
+// reseatear filtros
+const resetButton= $(".reset-btn")
+
+resetButton.addEventListener("click", () =>{
+    const filters= `brightness(1) opacity(1) contrast(100%) blur(0px) grayscale(0%) sepia(0%) hue-rotate(0deg) saturate(100%) invert(0)`;
+
+
+    $(".meme-image").style.filter= filters
+
+} )
+
+
+
+
+
+// Url como background
+const infoUrl= $("#url-img-input")
+const img= $(".meme-image")
+
+infoUrl.addEventListener("input", (e) => { 
+    img.style.backgroundImage= `url(${infoUrl.value})`
+}) 
